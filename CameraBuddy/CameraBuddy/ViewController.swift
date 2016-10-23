798dbac985eee621de74ad1415d96ed1a2c4bf5a
Crossroadsman <crossroadsman@gmail.com>
2016-10-23 06:50:30 +0100

    Update UI to have spinner selector for shutter

--- CameraBuddy/CameraBuddy/ViewController.swift
+++ CameraBuddy/CameraBuddy/ViewController.swift
@@ -9,7 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    // ------------------
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    // MARK: - IBOutlets
+    // -----------------
+    
+    
+    // MARK: - ViewController Standard Methods
+    // ---------------------------------------
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
